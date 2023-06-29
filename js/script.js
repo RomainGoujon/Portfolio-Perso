b5a502b52b7f6e19927290e9d53dfff91c07e84f
Romain Goujon <romaingoujon38@gmail.com>
2023-06-29 12:42:29 +0200

    Ajout boite modale

--- js/script.js
+++ js/script.js
@@ -56,6 +56,35 @@ function tabsFilters() {
 
 tabsFilters()
 
+function showProjectDetails() {
+  const links = document.querySelectorAll('.card_link');
+  const modals = document.querySelectorAll('.modal');
+  const btns = document.querySelectorAll('.modal_close');
+
+  const hideModals = () => {
+    modals.forEach(modal => {
+      modal.classList.remove('show');
+    });
+  }
+
+  links.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      document.querySelector(`[id=${elem.dataset.id}]`).classList.add('show');
+    });
+  });
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      hideModals();
+    });
+  });
+
+}
+
+showProjectDetails();
+
 // effets
 
 const observerIntersectionAnimation = () => {
